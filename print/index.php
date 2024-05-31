23d91f0cca78ce3f0b881bf59e46ac6d2e952aa3
menolakpajak <menolakpajak@gmail.com>
2024-06-01 00:06:33 +0800

    memperbaiki receive yang tidak bisa dibuka di page pickup

--- print/index.php
+++ print/index.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 
 ob_start();
 session_start();
-$page = ['print','../'];
+$page = ['print', '../'];
 
-if(empty($_SESSION['login'])){
-    header('Location: ../login');
+if (empty($_SESSION['login'])) {
+	header('Location: ../login');
 }
 
 require '../config.php';
 
 $kode = $_SESSION['kode'];
-login($kode,$_SESSION['token'],['master','admin','audit','cs']);
+login($kode, $_SESSION['token'], ['master', 'admin', 'audit', 'cs']);
 $userName = $_SESSION['user'];
 $kode = $_SESSION['kode'];
 $akses = $_SESSION['akses'];
 
-if(empty($_GET['id'])){
+if (empty($_GET['id'])) {
 	include_once '../error/index.php';
 	exit();
 }
@@ -24,19 +24,26 @@ if(empty($_GET['id'])){
 $id = $_GET['id'];
 
 $data = data("SELECT * FROM data WHERE no_spk = '$id'");
-if(!empty($data)){
+if (!empty($data)) {
 	include_once 'data.php';
 	die;
 }
 
+
 $data = data("SELECT * FROM invoice WHERE kode = '$id'");
-if(!empty($data)){
+if (!empty($data)) {
 	$data = $data[0];
-	if(!empty($data['link'])){
+	if (!empty($data['link'])) {
 		include_once 'invoice-for.php';
 		die;
-	}else{
+	} else {
 		include_once 'invoice.php';
 		die;
 	}
+}
+
+$data = data("SELECT * FROM pickup WHERE no_spk = '$id'");
+if (!empty($data)) {
+	include_once 'pickup.php';
+	die;
 }
